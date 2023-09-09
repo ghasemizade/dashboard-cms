78483245426650415ff1ba00aab9f51bc72dd00c
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-09 15:37:23 +0330

    upd index js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
+          <Header />
+          <App />
   </React.StrictMode>
 );
 
