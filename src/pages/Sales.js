b8f2c931632b353f90b79e2349d175ca0b30a845
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 15:21:16 +0330

    sales comp

---
+++ src/pages/Sales.js
@@ -0,0 +1,11 @@
+import clsx from 'clsx'
+import React from 'react'
+
+
+export default function Sales() {
+  return (
+      <div className={clsx(
+        "ml-80 pt-20"
+      )}>Sales</div>
+  )
+}
