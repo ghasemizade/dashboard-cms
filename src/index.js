12b391ef65882c908edcecb58a929f85632f335f
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 13:04:28 +0330

    browser router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-          <Header />
+      <>
+        {/* <Header /> */}
+        <BrowserRouter>
           <App />
-  </React.StrictMode>
+        </BrowserRouter>
+      </>
 );
 
 // If you want to start measuring performance in your app, pass a function
