2c4d9e808b281278e4aeaf5021af4a2fc4e56511
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 13:03:07 +0330

    import comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <Header />
+      <Home />
+
       <Sidebar />
     </>
   );
