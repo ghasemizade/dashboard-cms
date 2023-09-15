90fe42d2cba37612af12895c35ab7278004d10be
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 15:19:36 +0330

    import router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
+import router from "./Routes";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
-
+      {router}
       <Sidebar />
     </>
   );
