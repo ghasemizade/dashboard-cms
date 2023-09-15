ffef688df4e9587b3b95398a8309ce1e46cb9668
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 13:04:40 +0330

    home comp initial

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import clsx from 'clsx'
+import React from 'react'
+import Wrapper from '../components/wrapper'
+
+export default function Home() {
+  return (
+      <div className={clsx(
+        "ml-80 pt-20"
+      )}>Home</div>
+  )
+}
