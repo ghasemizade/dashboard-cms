db20280a9346cc62acb0942d2bfff44ade2eaab8
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 15:20:50 +0330

    newusers comp

---
+++ src/pages/Newusers.js
@@ -0,0 +1,11 @@
+import clsx from 'clsx'
+import React from 'react'
+
+
+export default function Newusers() {
+  return (
+      <div className={clsx(
+        "ml-80 pt-20"
+      )}>New Users</div>
+  )
+}
