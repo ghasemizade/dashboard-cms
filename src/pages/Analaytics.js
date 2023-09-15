d7a86afea996190f80fb2be567f70c3a95513ccc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-15 15:20:25 +0330

    analytics comp

---
+++ src/pages/Analaytics.js
@@ -0,0 +1,11 @@
+import clsx from 'clsx'
+import React from 'react'
+
+
+export default function Analaytics() {
+  return (
+      <div className={clsx(
+        "ml-80 pt-20"
+      )}>Analaytics</div>
+  )
+}
