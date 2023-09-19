e9a79c9f37fccf42e900e7e4c8b013476df7d98b
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-19 20:48:00 +0330

    update padding

--- src/components/SidebarSec.js
+++ src/components/SidebarSec.js
@@ -4,11 +4,11 @@ import clsx from 'clsx'
 export default function SidebarSec({title, children}) {
   return (
     <div className={clsx(
-        "pt-5 px-5",
+        "pt-4 px-5",
         "font-bold text-slate-400",
     )}>
         <p className={clsx(
-            'pb-3'
+            'pb-2'
         )}>{title}</p>
         {children}
     </div>
