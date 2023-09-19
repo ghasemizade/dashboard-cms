3e8e57b9132e046082133d123a2d863f2fc7bc02
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-19 20:48:13 +0330

    set strictmode

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <>
-        {/* <Header /> */}
+      <React.StrictMode>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </>
+      </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
