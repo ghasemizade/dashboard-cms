b39c161cd2b906c4de480f55a8dfd29b495c8298
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-20 18:56:40 +0330

    update (style): padding

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import React from 'react'
 export default function Home() {
   return (
       <div className={clsx(
-        "pl-80 pt-20"
       )}>Home lorem Home loremHome loremHome loremHome loremHome loremHome lorem</div>
   )
 }
