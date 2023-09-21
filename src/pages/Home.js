171e06da4a065a2aeda8025aee4d2815ecccc233
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-21 11:22:35 +0330

    update content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
 import clsx from 'clsx'
-import React from 'react'
 
 export default function Home() {
   return (
       <div className={clsx(
-      )}>Home lorem Home loremHome loremHome loremHome loremHome loremHome lorem</div>
+      )}>Home</div>
   )
 }
