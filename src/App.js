0115bfd280bea68a4ce664f58363fdc3305d5abb
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-21 11:21:08 +0330

    update name routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Sidebar from "./components/Sidebar";
-import routers from "./routes"
+import routersPath from "./routes"
 import { useRoutes } from "react-router-dom";
 import Header from "./components/Header";
 import clsx from "clsx";
@@ -7,17 +7,12 @@ import clsx from "clsx";
 
 function App() {
 
-  let router = useRoutes(routers)
+  let mainRouter = useRoutes(routersPath)
 
   return (
     <>
-      <Header />
-      <div className={clsx(
-        "flex",
-      )}>
-        <Sidebar />
-        {router}
-      </div>
+      <Header/>
+      {mainRouter}
     </>
   );
 }
