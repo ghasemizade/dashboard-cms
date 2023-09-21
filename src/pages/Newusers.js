1d3723002b1f2e74ffd40e7e0190270d478ef855
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-21 11:31:31 +0330

    remove (style): m & p

--- src/pages/Newusers.js
+++ src/pages/Newusers.js
@@ -4,8 +4,6 @@ import React from 'react'
 
 export default function Newusers() {
   return (
-      <div className={clsx(
-        "ml-80 pt-20"
-      )}>New Users</div>
+      <div>New Users</div>
   )
 }
