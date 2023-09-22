b9963d326ef929240727d36a41169d25200abc0d
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-22 11:35:09 +0330

    update (feature comp): implement features boxes component

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,18 +1,33 @@
 import clsx from "clsx";
 import FeaturesBox from "./FeaturesBox";
-
+import { BiDownArrowAlt, BiUpArrowAlt } from "react-icons/bi";
 
 export default function Features(){
 
     return(
         <div className={clsx(
-            "flex justify-evenly",
+            "md:flex md:justify-evenly md:flex-row flex flex-col",
             "w-full",
             "mt-10"
         )}>
-            <FeaturesBox />
-            <FeaturesBox />
-            <FeaturesBox />
+            <FeaturesBox 
+                title={"Revanue"} 
+                price={"2,125"} 
+                indicator={"-10.6"}
+                desc={"comapared to last month"}
+                indicatorIcon={<BiDownArrowAlt color="tomato"/>}/>
+            <FeaturesBox 
+                title={"Sales"} 
+                price={"4,025"} 
+                indicator={"-1.3"}
+                desc={"comapared to last month"}
+                indicatorIcon={<BiDownArrowAlt color="tomato"/>}/>
+            <FeaturesBox 
+                title={"Cost"} 
+                price={"2,325"} 
+                indicator={"+3.4"}
+                desc={"comapared to last month"}
+                indicatorIcon={<BiUpArrowAlt color="green"/>}/>
         </div>
     )
 }
