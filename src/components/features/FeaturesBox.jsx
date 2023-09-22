f280f9d4a361a9f1c7666d502f14028e8a2c1f05
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-22 10:48:36 +0330

    create (feature box)

---
+++ src/components/features/FeaturesBox.jsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+
+
+export default function FeaturesBox() {
+
+    return(
+        <div className={clsx(
+            "w-80 h-40",
+            "shadow hover:shadow-md",
+            "rounded"
+        )}>
+            
+        </div>
+    )
+}
