cb2a59eaa6aee8bc921f218c0406551d54f5fc3a
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-02 18:33:35 +0330

    update (flex)

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -8,26 +8,31 @@ export default function Features(){
         <div className={clsx(
             "md:flex md:justify-evenly md:flex-row flex flex-col",
             "w-full",
-            "mt-10",
+            "mt-7 mx-0",
         )}>
-            <FeaturesBox 
-                title={"Revanue"} 
-                price={"2,125"} 
-                indicator={"-10.6"}
-                desc={"comapared to last month"}
-                indicatorIcon={<BiDownArrowAlt color="tomato"/>}/>
-            <FeaturesBox 
-                title={"Sales"} 
-                price={"4,025"} 
-                indicator={"-1.3"}
-                desc={"comapared to last month"}
-                indicatorIcon={<BiDownArrowAlt color="tomato"/>}/>
-            <FeaturesBox 
-                title={"Cost"} 
-                price={"2,325"} 
-                indicator={"+3.4"}
-                desc={"comapared to last month"}
-                indicatorIcon={<BiUpArrowAlt color="green"/>}/>
+            <div className={clsx(
+                "flex justify-center",
+                "w-full"
+            )}>
+                <FeaturesBox 
+                    title={"Revanue"} 
+                    price={"2,125"} 
+                    indicator={"-10.6"}
+                    desc={"comapared to last month"}
+                    indicatorIcon={<BiDownArrowAlt color="tomato"/>}/>
+                <FeaturesBox 
+                    title={"Sales"} 
+                    price={"4,025"} 
+                    indicator={"-1.3"}
+                    desc={"comapared to last month"}
+                    indicatorIcon={<BiDownArrowAlt color="tomato"/>}/>
+                <FeaturesBox 
+                    title={"Cost"} 
+                    price={"2,325"} 
+                    indicator={"+3.4"}
+                    desc={"comapared to last month"}
+                    indicatorIcon={<BiUpArrowAlt color="green"/>}/>
+            </div>
         </div>
     )
 }
