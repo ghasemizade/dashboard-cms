35d5b229992071f3572f0bf60d69cf502ac0de8c
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-03 14:10:58 +0330

    update (axis data)

--- src/components/charts/data.jsx
+++ src/components/charts/data.jsx
@@ -1,42 +1,72 @@
 const data = [
     {
-      "name": "Page A",
+      "name": "Jan",
       "uv": 4000,
       "pv": 2400,
       "amt": 2400
     },
     {
-      "name": "Page B",
+      "name": "Feb",
       "uv": 3000,
       "pv": 1398,
       "amt": 2210
     },
     {
-      "name": "Page C",
+      "name": "Mar",
       "uv": 2000,
       "pv": 9800,
       "amt": 2290
     },
     {
-      "name": "Page D",
+      "name": "Apr",
       "uv": 2780,
       "pv": 3908,
       "amt": 2000
     },
     {
-      "name": "Page E",
+      "name": "May",
       "uv": 1890,
       "pv": 4800,
       "amt": 2181
     },
     {
-      "name": "Page F",
+      "name": "Jun",
       "uv": 2390,
       "pv": 3800,
       "amt": 2500
     },
     {
-      "name": "Page G",
+      "name": "Jul",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    },
+    {
+      "name": "Aug",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    },
+    {
+      "name": "Sep",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    },
+    {
+      "name": "Oct",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    },
+    {
+      "name": "Nov",
+      "uv": 3490,
+      "pv": 4300,
+      "amt": 2100
+    },
+    {
+      "name": "Dec",
       "uv": 3490,
       "pv": 4300,
       "amt": 2100
