278d6b5faa27d09898b39a7f5b2d14f09c13a8be
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-04 13:57:19 +0330

    import comp

--- src/components/transactions/Lasttransaction.jsx
+++ src/components/transactions/Lasttransaction.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import clsx from 'clsx'
-import avatar from '../../assets/avatar1.jpg'
+import Customerrow from './Customerrow'
+import avatar1 from '../../assets/avatar1.jpg'
+import avatar2 from '../../assets/avatar2.jpg'
+import avatar3 from '../../assets/avatar3.jpg'
 
 export default function Lasttransaction() {
   return (
@@ -27,43 +30,32 @@ export default function Lasttransaction() {
               <th>Amount</th>
               <th>Status</th>
             </tr>
-            <tr>
-              <td className={clsx(
-                "flex items-center",
-              )}>
-                <img 
-                  className={clsx(
-                    "rounded-full",
-                    "mr-4"
-                  )}
-                  src={avatar} 
-                  alt="avatar" 
-                  width={40}/>
-                  <p className={clsx(
-                    "text-slate-500"
-                  )}>Susan Carol</p>
-              </td>
-              <td>
-                <p className={clsx(
-                    "text-slate-400"
-                  )}>2 Jun 2023</p>
-              </td>
-              <td>
-                <p className={clsx(
-                    "text-slate-400"
-                  )}>$ 99.00</p>
-              </td>
-              <td>
-                <div className={clsx(
-                    "text-xs",
-                    "text-green-500",
-                    "bg-green-100",
-                    "py-1 px-2",
-                    "rounded-full",
-                    "w-fit"
-                  )}>Approved</div>
-              </td>
-            </tr>
+            <Customerrow 
+              cusName={"Sauson Carol"}
+              avatar={avatar1}
+              date={"4 Jul 2023"}
+              amount={"99.00"}
+              status={"Approved"}
+              textStatus={"text-green-500"}
+              bgStatus={"bg-green-100"}/>
+
+              <Customerrow 
+              cusName={"Sarah Carol"}
+              avatar={avatar2}
+              date={"10 Jul 2023"}
+              amount={"120.00"}
+              status={"Declined"}
+              textStatus={"text-red-500"}
+              bgStatus={"bg-red-100"}/>
+
+              <Customerrow 
+              cusName={"Sauson Carol"}
+              avatar={avatar3}
+              date={"15 Jul 2023"}
+              amount={"59.00"}
+              status={"Pending"}
+              textStatus={"text-blue-500"}
+              bgStatus={"bg-blue-100"}/>
           </table>
         </div>
     </div>
