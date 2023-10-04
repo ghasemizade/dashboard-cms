ffda5c46f8ecd5eb27942f992f12aa1958881f50
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-04 13:27:20 +0330

    feat (customer table)

--- src/components/transactions/Lasttransaction.jsx
+++ src/components/transactions/Lasttransaction.jsx
@@ -1,7 +1,71 @@
 import React from 'react'
+import clsx from 'clsx'
+import avatar from '../../assets/avatar1.jpg'
 
 export default function Lasttransaction() {
   return (
-    <div></div>
+    <div className={clsx(
+      "bg-slate-50",
+      "shadow",
+      "rounded",
+      "p-2 ml-3",
+      "w-2/3"
+  )}>
+    <h3 className={clsx(
+            "text-2xl font-semibold"
+        )}>Latest Transactions</h3>
+        <div className={clsx(
+          "my-8 mx-5"
+        )}>
+          <table className={clsx(
+            "text-left",
+            "w-full",
+          )}>
+            <tr>
+              <th>Customer</th>
+              <th>Date</th>
+              <th>Amount</th>
+              <th>Status</th>
+            </tr>
+            <tr>
+              <td className={clsx(
+                "flex items-center",
+              )}>
+                <img 
+                  className={clsx(
+                    "rounded-full",
+                    "mr-4"
+                  )}
+                  src={avatar} 
+                  alt="avatar" 
+                  width={40}/>
+                  <p className={clsx(
+                    "text-slate-500"
+                  )}>Susan Carol</p>
+              </td>
+              <td>
+                <p className={clsx(
+                    "text-slate-400"
+                  )}>2 Jun 2023</p>
+              </td>
+              <td>
+                <p className={clsx(
+                    "text-slate-400"
+                  )}>$ 99.00</p>
+              </td>
+              <td>
+                <div className={clsx(
+                    "text-xs",
+                    "text-green-500",
+                    "bg-green-100",
+                    "py-1 px-2",
+                    "rounded-full",
+                    "w-fit"
+                  )}>Approved</div>
+              </td>
+            </tr>
+          </table>
+        </div>
+    </div>
   )
 }
